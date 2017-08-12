7aeb9515bad58f68643b42b9f87f7b2ee618d1db
guardian-of-kalidore <unicornsofkalidore@gmail.com>
2017-08-11 22:20:07 -0400

    Adding in the Cross Origin ability

--- src/main/java/com/kalidore/herokumicro/WebserviceControllerProto.java
+++ src/main/java/com/kalidore/herokumicro/WebserviceControllerProto.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,7 +31,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  *
  * @author ahill
  */
-//@CrossOrigin // Only enable if you want to use this to prototype local demos
+@CrossOrigin // Only enable if you want to use this to prototype local demos
 @Controller
 @EnableAutoConfiguration
 public class WebserviceControllerProto {
